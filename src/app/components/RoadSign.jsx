471e47a41f86f01493006a0d5608645fd36499bc
Petr <petemashine@gmail.com>
2022-12-04 22:33:30 +0300

    trying fix problem with timeout

--- src/app/components/RoadSign.jsx
+++ src/app/components/RoadSign.jsx
@@ -1,36 +1,97 @@
-import React, { useState } from "react";
+// import React, { useState , useEffect , useRef } from "react";
 
-export default function RoadSign(props) {
-    const [counter, setCounter] = useState(1);
-    const [sign, setSign] = useState('ready');
-
-    const gameResult = props.gameResult;
+// export default function RoadSign(props) {
     
-    let output ;
+//     const [counter, setCounter] = useState(1);
+    
+//     const [sign, setSign] = useState('ready');
 
-    if (gameResult === "in Process") {
-        let way = props.way;
-        let timer = setTimeout(() => {
-            if (counter === way.length + 1) {
-                setSign("finish");
-                clearTimeout(timer);
-                return;
-            }
-            setSign(` step ${counter} ${way[counter - 1]}`);
-            setCounter(counter + 1);
-        }, 1000);
+//     const gameResult = props.gameResult;
 
-        // if (counter === way.length + 1 ) {
-            // clearTimeout(timer);
-        // }
 
-        output = sign ;
-    } else {
-        output = gameResult ;
+//     useEffect(() => {
+        
+//     }) ;
 
-    }
 
-    return  <div className = {`text-7xl font-extrabold mb-[2rem] `}>
-        {output}
-    </div>
+
+
+//     function waySign () {
+ 
+//     if (gameResult === "in Process") {
+//         let way = props.way;
+//         setTimeout(() => {
+//             if (counter === way.length + 1) {
+//                 setSign("finish");
+//                 return;
+//             }
+//             setSign(`step ${counter} ${way[counter - 1]}`);
+//             setCounter(counter + 1);
+//         }, 1000);
+
+//         return sign ;
+//     } else {
+//         return gameResult ;
+
+//     }
+// }
+
+//     return  <div className = {`text-7xl font-extrabold mb-[2rem] `}>
+//         {waySign()}
+//     </div>
+// }
+
+
+
+
+import React, { useState , useEffect , useRef } from "react";
+
+export default function RoadSign( {way , gameResult} ) {
+    
+    const counterRef = useRef(1) ;
+
+    const timerRef = useRef('') ;
+
+    const clearTimeoutRef = useRef(false) ;
+    
+    const signRef = useRef('ready') ;
+
+    const [ sign , setSign ] = useState('ready') ;
+   
+    console.log(way[counterRef.current]) ;
+    
+    counterRef.current += 1 ;
+    
+    console.log(way[counterRef.current]) ;
+
+    useEffect ( () => {
+        if (counterRef.current <= way.length-1) {
+                timerRef.current = setTimeout(()=>{
+                if(counterRef.current === way.length-1){
+                    setSign(`finish`) ;
+                    return ;
+                }else{
+                    setSign(`step ${ counterRef.current } --- ${ way[counterRef.current - 1] }`) ;
+                }
+                
+                clearTimeoutRef.current = !clearTimeoutRef.current ;
+            } , 1000) ;
+        }else if ( gameResult !== 'in Process') {
+            setSign(gameResult) ;
+        }
+    } ) ;
+
+    useEffect(() => {
+        clearTimeout(clearTimeoutRef.current) ;
+    } , [clearTimeoutRef]) ;
+
+
+    return (
+        <div>
+            {sign}
+        </div>
+    )
+
+
+   
 }
--- src/app/components/StartPage/StartPage.jsx
+++ src/app/components/StartPage/StartPage.jsx
@@ -1,70 +1,113 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { PlusCircleIcon, MinusCircleIcon, ChevronDownIcon } from '@heroicons/react/outline'
+import {
+  PlusCircleIcon,
+  MinusCircleIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/outline";
 
-import { changeSide, changeSteps, gameOver } from "../../slices/gameOptionSlice";
+import {
+  changeSide,
+  changeSteps,
+  gameOver,
+} from "../../slices/gameOptionSlice";
 
 export default function StartPage() {
-    let sideStore = useSelector(state => state.gameOption.side);
-    let stepsStore = useSelector(state => state.gameOption.steps);
-    const dispatch = useDispatch();
+  let sideStore = useSelector((state) => state.gameOption.side);
+  let stepsStore = useSelector((state) => state.gameOption.steps);
+  const dispatch = useDispatch();
 
-    const [listVisible, setListVisible] = useState(false);
+  const [listVisible, setListVisible] = useState(false);
 
-    return <div className="flex items-start flex-col mx-auto w-[80%] h-[60%] mt-10 ">
+  return (
+    <div className="flex items-start flex-col mx-auto w-[80%] h-[60%] mt-10 ">
+      <h2 className="font-bold text-8xl font-sans tracking-widest">Лабиринт</h2>
 
-        <h2 className="font-bold text-8xl font-sans tracking-widest">Лабиринт</h2>
+      <div className="mt-[5rem]">
+        <h2 className="text-5xl font-sans tracking-widest underline">
+          Задайте сложность игры
+        </h2>
 
-        <div className="mt-[5rem]">
-            <h2 className="text-5xl font-sans tracking-widest underline">Задайте сложность игры</h2>
-
-            <div className="mt-[3rem]">
-                <span className="flex space-x-10 items-center">
-                    <span className="cursor-help text-5xl" title="(не менее 3 и не более 15)">
-                        Сторона поля :
-                    </span>
-                    <button className="rounded-full w-[5rem] h-[5rem]"
-                        onClick={() => dispatch(changeSide("decrease"))}>
-                        <MinusCircleIcon />
-                    </button>
-                    <span className = "text-5xl" >{sideStore}</span>
-                    <button className="rounded-full w-[5rem] h-[5rem]"
-                        onClick={() => dispatch(changeSide("increase"))} >
-                        <PlusCircleIcon />
-                    </button>
-                </span>
-            </div>
-            <div className="mt-[3rem]" >
-                <span className="flex space-x-10 items-center">
-                    <span className="cursor-help text-5xl" title="(не менее 1 и не более 15)">
-                        Количество ходов :
-                    </span>
-                    <button className="rounded-full w-[5rem] h-[5rem]"
-                        onClick={() => dispatch(changeSteps("decrease"))}>
-                        <MinusCircleIcon />
-                    </button>
-                    <span className = "text-5xl" >{stepsStore}</span>
-                    <button className="rounded-full w-[5rem] h-[5rem]"
-                        onClick={() => dispatch(changeSteps("increase"))}>
-                        <PlusCircleIcon />
-                    </button>
-                </span>
-            </div>
+        <div className="mt-[3rem]">
+          <span className="flex space-x-10 items-center">
+            <span
+              className="cursor-help text-5xl"
+              title="(не менее 3 и не более 15)"
+            >
+              Сторона поля :
+            </span>
+            <button
+              className="rounded-full w-[5rem] h-[5rem]"
+              onClick={() => dispatch(changeSide("decrease"))}
+            >
+              <MinusCircleIcon />
+            </button>
+            <span className="text-5xl">{sideStore}</span>
+            <button
+              className="rounded-full w-[5rem] h-[5rem]"
+              onClick={() => dispatch(changeSide("increase"))}
+            >
+              <PlusCircleIcon />
+            </button>
+          </span>
         </div>
-        <button className="rounded text-6xl border-[10px] border-black w-[100%] h-[6rem]  mt-[7rem]"
-            onClick={() => dispatch(gameOver(false))}>
-            Начать
-        </button>
-
-
-        <div>
-            <ul className="flex items-center">Правила игры
-                <button onClick={() => setListVisible(!listVisible)} className="ml-2 w-4 h-4 "><ChevronDownIcon /></button></ul>
-            {listVisible ? <>
-                <li> Необходимо на поле , мысленно следуя инструкциям , передвигаться из клетки старта (отмеченая клетка) к финишу</li>
-                <li> После последней инструкции выбрать клетку финиш</li>
-            </> : <></>}
-
+        <div className="mt-[3rem]">
+          <span className="flex space-x-10 items-center">
+            <span
+              className="cursor-help text-5xl"
+              title="(не менее 1 и не более 15)"
+            >
+              Количество ходов :
+            </span>
+            <button
+              className="rounded-full w-[5rem] h-[5rem]"
+              onClick={() => dispatch(changeSteps("decrease"))}
+            >
+              <MinusCircleIcon />
+            </button>
+            <span className="text-5xl">{stepsStore}</span>
+            <button
+              className="rounded-full w-[5rem] h-[5rem]"
+              onClick={() => dispatch(changeSteps("increase"))}
+            >
+              <PlusCircleIcon />
+            </button>
+          </span>
         </div>
+      </div>
+      <button
+        className="rounded text-6xl border-[10px] border-black w-[100%] h-[6rem]  mt-[7rem]"
+        onClick={() => dispatch(gameOver(false))}
+      >
+        Начать
+      </button>
+
+      <div>
+        <ul className="flex items-center text-4xl mt-[4rem]">
+          Правила игры
+          <button
+            onClick={() => setListVisible(!listVisible)}
+            className="ml-2 w-4 h-4 "
+          >
+            <ChevronDownIcon />
+          </button>
+        </ul>
+        {listVisible ? (
+          <>
+            <li className="text-3xl">
+              {" "}
+              Необходимо на поле , мысленно следуя инструкциям , передвигаться
+              из клетки старта (отмеченая клетка) к финишу
+            </li>
+            <li className="text-3xl">
+              {" "}
+              После последней инструкции выбрать клетку финиш
+            </li>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
+  );
 }
