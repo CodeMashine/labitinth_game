5c44cb583dfb0e3c12ac48387d5c5e8cf4e1b84e
Peter <79867740+CodeMashine@users.noreply.github.com>
2022-12-30 14:33:10 +0300

    fix : fix start page for mobile devices

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux" ;
 
 export default function App() {
   const isEnd = useSelector(state => state.gameOption.isEnd) ; 
-  return (<div className = "flex w-screen h-screen md:w-[60%] md:mx-auto">
+  return (<div className = "flex w-[80%] h-screen mx-auto">
     { isEnd ? <StartPage/> :<GameField/> }
   </div>)
 }
--- src/app/components/StartPage/StartPage.jsx
+++ src/app/components/StartPage/StartPage.jsx
@@ -20,70 +20,87 @@ export default function StartPage() {
   const [listVisible, setListVisible] = useState(false);
 
   return (
-    <div className="flex items-start flex-col mx-auto w-[80%] h-[60%] mt-10 md:w-[60%] ">
-      <h2 className="font-bold text-8xl md:text-6xl font-sans tracking-widest">Лабиринт</h2>
+    <div className="flex items-start flex-col mx-auto w-[100%] h-[100%] pt-[20rem]  ">
 
-      <div className="mt-[5rem]">
-        <h2 className="text-5xl md:text-3xl font-sans tracking-widest underline">
+      <h2 className="font-bold text-8xl font-sans tracking-widest">Лабиринт</h2>
+
+
+
+{/* settings block */}
+      <div className="mt-[5rem] w-[100%]">
+        <h2 className="text-5xl font-sans tracking-widest underline">
           Задайте сложность игры
         </h2>
 
-        <div className="mt-[3rem]">
-          <span className="flex space-x-10 items-center">
+
+{/* сторона поля */}
+        {/* <div className="mt-[3rem]"> */}
+          <div className="flex mt-[3rem] justify-between  items-center">
             <span
-              className="cursor-help text-5xl md:text-3xl"
+              className="cursor-help text-5xl"
               title="(не менее 3 и не более 15)"
             >
               Сторона поля :
             </span>
+
+
+            {/* button block */}
+            <div className = "flex items-center justify-between w-[38%]">
             <button
               className="rounded-full w-[5rem] h-[5rem]"
               onClick={() => dispatch(changeSide("decrease"))}
-            >
+              >
               <MinusCircleIcon />
             </button>
-            <span className="text-5xl md:text-3xl">{sideStore}</span>
+            <span className="text-5xl">{sideStore}</span>
             <button
               className="rounded-full w-[5rem] h-[5rem]"
               onClick={() => dispatch(changeSide("increase"))}
-            >
+              >
               <PlusCircleIcon />
             </button>
-          </span>
+            </div>
         </div>
-        <div className="mt-[3rem]">
-          <span className="flex space-x-10 items-center">
+      {/* </div> */}
+
+{/* количество ходов */}
+<div className="flex mt-[3rem] justify-between  items-center">
             <span
-              className="cursor-help text-5xl md:text-3xl"
+              className="cursor-help text-5xl"
               title="(не менее 1 и не более 15)"
             >
               Количество ходов :
             </span>
+
+{/* button block */}
+         <div className = "flex items-center justify-between w-[38%]">
             <button
               className="rounded-full w-[5rem] h-[5rem]"
               onClick={() => dispatch(changeSteps("decrease"))}
             >
               <MinusCircleIcon />
             </button>
-            <span className="text-5xl md:text-3xl">{stepsStore}</span>
+            <span className="text-5xl">{stepsStore}</span>
             <button
               className="rounded-full w-[5rem] h-[5rem]"
               onClick={() => dispatch(changeSteps("increase"))}
             >
               <PlusCircleIcon />
             </button>
-          </span>
+          </div>
         </div>
       </div>
+
+      {/* start button */}
       <button
-        className="rounded text-6xl md:text-4xl border-[10px] border-black w-[100%] h-[6rem]  mt-[7rem] md:mt-[4rem]"
+        className="rounded text-6xl  border-[10px] border-black w-[100%] h-[6rem]  mt-[7rem] "
         onClick={() => dispatch(gameOver(false))}
       >
         Начать
       </button>
 
       <div>
-        <ul className="flex items-center text-4xl md:text-3xl mt-[4rem]">
+        <ul className="flex items-center text-4xl mt-[4rem]">
           Правила игры
           <button
             onClick={() => setListVisible(!listVisible)}
@@ -94,11 +111,11 @@ export default function StartPage() {
         </ul>
         {listVisible ? (
           <>
-            <li className="text-3xl md:text-2xl">
+            <li className="text-3xl">
               Необходимо на поле , мысленно следуя инструкциям , передвигаться
               из клетки старта (отмеченая клетка) к финишу
             </li>
-            <li className="text-3xl md:text-2xl">
+            <li className="text-3xl">
               После последней инструкции выбрать клетку финиш
             </li>
           </>
